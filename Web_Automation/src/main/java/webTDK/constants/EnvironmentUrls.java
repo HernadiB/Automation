dd6534648fc906ba3fd0c6ad88566996f042a9a7
HernadiB <hernadi.barnabas39@gmail.com>
2024-04-02 18:10:27 +0200

    environment urls added

--- Web_Automation/src/main/java/webTDK/constants/EnvironmentUrls.java
+++ Web_Automation/src/main/java/webTDK/constants/EnvironmentUrls.java
@@ -2,6 +2,85 @@ package webTDK.constants;
 
 public class EnvironmentUrls {
 
-    /** Grand Casino **/
+    //region Magyar kaszinók
+    /** Grand Casino **/ //!!!!!!!
     public static final String GRANDCASINO_URL = "https://grandcasino.hu/hu-hu/";
+
+    /** Vegas Casino **/
+    public static final String VEGASCASINO_URL = "https://vegas.hu/";
+
+    /** Vegas Casino VIP **/
+    public static final String VEGASCASINOVIP_URL = "https://vipcasino.hu/";
+
+    //endregion
+
+    //region Svéd kaszinók
+
+    /** Vera&John **/ //!!!!!!!
+    public static final String VERAJOHN_URL = "https://www.verajohn.se/en";
+
+    /** Expekt **/ //VPN !!!!!!!!
+    public static final String EXPEKT_URL = "https://www.expekt.se/";
+
+    /** Hajper **/ //VPN !!!!!!!!
+    public static final String HAJPER_URL = "https://hajper.se/";
+
+    /** Paf **/ //!!!!!!!
+    public static final String PAF_URL = "https://www.paf.se/en";
+
+    /** AB Trav and Galopp **/
+    public static final String TRAVANDGALOPP_URL = "https://www.atg.se/";
+
+    /** 888 Sweden Limited **/
+    public static final String SWEDENLIMITED_URL = "https://www.888casino.se/";
+
+    /** Auto Nordic Plc **/ //VPN
+    public static final String AUTONORDIC_URL = "https://jallacasino.se/";
+
+    /** Betfair International Ltd **/
+    public static final String BETFAIRINTERNATIONAL_URL = "https://www.betfair.se/hu";
+
+    /** Blue Star Planet Limited **/
+    public static final String BLUESTARTLIMITED_URL = "https://www.10bet.se/";
+
+    /** Lyckost ApS **/
+    public static final String LYCKOST_URL = "https://www.lyckost.se/";
+
+    //endregion
+
+    //region Szlovák kaszinók
+
+    /** NIKÉ **/ //!!!!!!!
+    public static final String NIKE_URL = "https://www.nike.sk/";
+
+    /** Tipsport **/ //!!!!!!!
+    public static final String TIPSPORT_URL = "https://www.tipsport.sk/";
+
+    /** Fortuna **/
+    public static final String FORTUNA_URL = "https://www.ifortuna.sk/";
+
+    /** Tipos **/ //!!!!!!!
+    public static final String TIPOS_URL = "https://moj.etipos.sk/";
+
+    /** Doxxbet **/
+    public static final String DOXXBET_URL = "https://www.doxxbet.sk/";
+
+    /** Monacobet **/
+    public static final String MONACOBET_URL = "https://casino.monacobet.sk/";
+
+    /** Kajotwin **///!!!!!!!
+    public static final String KAJOTWIN_URL = "https://www.kajotwin.sk/sk/";
+
+    /** Betor **/
+    public static final String BETOR_URL = "https://www.betor.sk/";
+
+    /** Synottip **/ //VPN
+    public static final String SYNOTTIP_URL = "https://synottip.sk/";
+
+    /** OlyBet **/ //!!!!!!!
+    public static final String OLYBET_URL = "https://olybet.sk/";
+
+
+    //endregion
+
 }
