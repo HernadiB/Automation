0d5b772f46e127c7e799c4183cf83181e9245045
HernadiB <hernadi.barnabas39@gmail.com>
2024-04-02 14:19:36 +0200

    RandomHelper doc and faker

--- Web_Automation/src/main/java/webTDK/common/helpers/RandomHelper.java
+++ Web_Automation/src/main/java/webTDK/common/helpers/RandomHelper.java
@@ -1,5 +1,6 @@
 package webTDK.common.helpers;
 
+import com.github.javafaker.Faker;
 import org.apache.commons.lang3.RandomStringUtils;
 
 import java.io.BufferedReader;
@@ -7,18 +8,22 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.time.LocalDate;
+import java.time.Year;
 import java.time.temporal.ChronoUnit;
-import java.util.ArrayList;
-import java.util.GregorianCalendar;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 public class RandomHelper {
+    public static Faker faker = new Faker(new Locale("hu"));
+    public int yearNow = Year.now().getValue();
 
-    //TODO: későbbiekben ilyen formában lesz a NamePoolok felöltése.
-    public List<String> readData(){
+
+    /**
+     * Read the zip numbers from an external .scv file
+     * @return List of strings containing the data read from the .csv file
+     */
+    public List<String> readZipNumbersData(){
         List<String[]> content = new ArrayList<>();
-        try (BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream("C:\\Users\\geveres\\Documents\\zip_numbers.csv"), "ISO-8859-1"))) {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(RandomHelper.class.getResourceAsStream("/zip_numbers.csv"), "ISO-8859-1"))) {
             String line = "";
             while ((line = br.readLine()) != null) {
                 content.add(line.split(";"));
@@ -35,24 +40,69 @@ public class RandomHelper {
         return result;
     }
 
+    /**
+     * Weighted random number generation
+     * @param min The lower limit of interval
+     * @param max The upper limit of interval
+     * @param target The target value towards which the method is based
+     * @param weight Probability of returning the target value
+     * @return A number returned between the lower and upper limits of the interval, based on a certain frequently of a target value
+     */
+    public static int weightedRandomNumber(int min, int max, int target, double weight){
+        Random random = new Random();
+        double randomNumber = random.nextDouble();
+        if (randomNumber < weight) {
+            return target;
+        } else {
+            return random.nextInt((max - min) + 1) + min;
+        }
+    }
+
+    /**
+     * Randomly selects a prefix string from a predefined pool of name prefixes
+     * @return Selected prefix from the predefined
+     */
     public String generatePrefix(){
         String[] NamePool = {"dr.", "id.", "ifj.", "özv.", "prof.", ""};
-        int rnd = new Random().nextInt(NamePool.length);
+        int rnd = weightedRandomNumber(0, 5, 5, 0.5);
         return NamePool[rnd];
     }
 
+    /**
+     * Generateing a full name using an external library
+     * @return Full name prefixed with "TA-"
+     */
+    public String generateFullName(){
+        String lastName = "TA-" + faker.name().lastName();
+        String firstName = faker.name().lastName();
+        String fullName = lastName + " " + firstName;
+        return fullName;
+    }
+
+    /**
+     * Extracing the first part of a generated full name string
+     * @return Separated first name
+     */
     public String generateFirstName(){
-        String[] NamePool = {"László", "test"};
-        int rnd = new Random().nextInt(NamePool.length);
-        return NamePool[rnd];
+        String[] parts = generateFullName().split(" ");
+        String firstName = parts[0];
+        return firstName;
     }
 
-    public String generateLastName(){
-        String[] NamePool = {"Nagy", "test"};
-        int rnd = new Random().nextInt(NamePool.length);
-        return "TA-" + NamePool[rnd];
+    /**
+     * Extracing the second part of a generated full name string
+     * @return Separated last name
+     */
+    public String generateLastName() {
+        String[] parts = generateFullName().split(" ");
+        String lastName = parts[1];
+        return lastName;
     }
 
+    /**
+     * Generating a random birthdate string in the format "YYYY.MM-DD." within a specified range
+     * @return Birthdate "YYYY-MM.DD." format
+     */
     //TODO: refact the double digit month and days
     public String generateBirthDate(){
         GregorianCalendar gc = new GregorianCalendar();
@@ -71,54 +121,87 @@ public class RandomHelper {
         return gc.get(gc.YEAR) + "." + month + "." + day + ".";
     }
 
+    /**
+     * Generating a radnom number from a certain interval
+     * @param start The lower limit of interval
+     * @param end The upper limit of interval
+     * @return Random number generated between the lower and upper limit of the interval
+     */
     public static int randBetween(int start, int end){
         return start + (int)Math.round(Math.random() * (end - start));
     }
 
+    /**
+     * Randomly selects a birthplace from the pool
+     * @return Selected birthplace
+     */
     public String generateBirthPlace(){
         String[] NamePool = {"Budapest", "Debrecen"};
         int rnd = new Random().nextInt(NamePool.length);
         return NamePool[rnd];
     }
 
-    //TODO: upgrade needed
+    /**
+     * Generating a valid date string in the format "YYYY.MM.DD." based on specific target
+     * @return Generated date in the format "YYYY.MM.DD."
+     */
     public String generateDocumentValidFrom(){
         GregorianCalendar gc = new GregorianCalendar();
-        int year = randBetween(2015, 2021);
+        int year = randBetween(yearNow - 8, yearNow -2);
         gc.set(gc.YEAR, year);
         int dayOfYear = randBetween(1, gc.getActualMaximum(gc.DAY_OF_YEAR));
         gc.set(gc.DAY_OF_YEAR, dayOfYear);
         return gc.get(gc.YEAR) + "." + (gc.get(gc.MONTH) + 1) + "." + gc.get(gc.DAY_OF_MONTH) + ".";
     }
 
-    //TODO: upgrade needed
+    /**
+     * Generating a valid date string in the format "YYYY.MM.DD." based on specific target
+     * @return Generated date in the format "YYYY.MM.DD."
+     */
     public String generateDocumentValidTo(){
         GregorianCalendar gc = new GregorianCalendar();
-        int year = randBetween(2023, 2030);
+        int year = randBetween(yearNow + 1, yearNow + 8);
         gc.set(gc.YEAR, year);
         int dayOfYear = randBetween(1, gc.getActualMaximum(gc.DAY_OF_YEAR));
         gc.set(gc.DAY_OF_YEAR, dayOfYear);
         return gc.get(gc.YEAR) + "." + (gc.get(gc.MONTH) + 1) + "." + gc.get(gc.DAY_OF_MONTH) + ".";
     }
 
+    /**
+     * Generating a random string that combines six digits and two uppercase alphabetic characters
+     * @return String that consists of 6 random digits 2 randomly generated uppercase alphabetic characters
+     */
     public String generate6and2Number(){
         String rndNum = RandomStringUtils.randomNumeric(6);
         String rndAlp = RandomStringUtils.randomAlphabetic(2).toUpperCase();
         return rndNum + rndAlp;
     }
 
+    /**
+     * Randomly selecting and returning a string representing a zip code and city
+     * @return Selected zip code and city
+     */
     public String generateZipCodeAndCity(){
-        List<String> NamePool = readData();
+        List<String> NamePool = readZipNumbersData();
         Random random = new Random();
         String rnd = NamePool.get(random.nextInt(NamePool.size()));
         return rnd;
     }
 
+    /**
+     * Generating a random house number as a string within the range of 0 tp 99
+     * @return Randomly generated house number
+     */
     public String generateHouseNumber(){
         String rnd = Integer.toString(randBetween(0, 99));
         return rnd;
     }
 
+    /**
+     *
+     * @param birthDate Input parameter is a date of birth
+     * @return Generated Hungarian tax number based on the given date of birth
+     */
     public String generateHunTayNumber(String birthDate){
         /*
         1. szám fix 8
