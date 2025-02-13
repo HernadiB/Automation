5f61442dcb5f093ba7d6d90224841b50dd01494f
HernadiB <hernadi.barnabas39@gmail.com>
2025-02-13 20:31:52 +0100

    ATG test

--- Web_Automation/src/main/java/automationBase/AutomationUI.java
+++ Web_Automation/src/main/java/automationBase/AutomationUI.java
@@ -3,11 +3,13 @@ package automationBase;
 
 import casinoPages.hungarian_casino.grandCasinoPages.GrandCasinoLandingPage;
 import casinoPages.slovak_casino.nikePages.NikeLandingPage;
+import casinoPages.sweden_casino.atgPages.AtgLandingPage;
 import casinoPages.sweden_casino.expektPages.ExpektLandingPage;
 import casinoPages.sweden_casino.hajperPages.HajperLandingPage;
 import casinoPages.sweden_casino.pafPages.PafLandingPage;
 import casinoPages.hungarian_casino.vegasCasinoPages.VegasCasinoLandingPage;
 import casinoPages.hungarian_casino.vegasCasinoVipPages.VegasCasinoVipLandingPage;
+import casinoPages.slovak_casino.tipSportPages.TipSportLandingPage;
 import casinoPages.sweden_casino.travAndGaloppPages.TravAndGaloppLandingPage;
 import casinoPages.sweden_casino.veraJohnPages.VeraJohnLandingPage;
 
@@ -22,6 +24,8 @@ public class AutomationUI {
     public HajperLandingPage hajperLandingPage;
     public TravAndGaloppLandingPage travAndGaloppLandingPage;
     public NikeLandingPage nikeLandingPage;
+    public TipSportLandingPage tipSportLandingPage;
+    public AtgLandingPage atgLandingPage;
 
     public AutomationUI(){
         this.grandCasinoLandingPage = new GrandCasinoLandingPage();
@@ -33,5 +37,7 @@ public class AutomationUI {
         this.hajperLandingPage = new HajperLandingPage();
         this.travAndGaloppLandingPage = new TravAndGaloppLandingPage();
         this.nikeLandingPage = new NikeLandingPage();
+        this.tipSportLandingPage = new TipSportLandingPage();
+        this.atgLandingPage = new AtgLandingPage();
     }
 }
--- Web_Automation/src/main/java/webTDK/constants/EnvironmentUrls.java
+++ Web_Automation/src/main/java/webTDK/constants/EnvironmentUrls.java
@@ -16,33 +16,42 @@ public class EnvironmentUrls {
 
     //region Svéd kaszinók
 
+    //TODO
     /** Vera&John **/
     public static final String VERAJOHN_URL = "https://www.verajohn.se/en";
 
+    //TODO
     /** Expekt **/ //VPN !!!!!!!!
     public static final String EXPEKT_URL = "https://www.expekt.se/";
 
+    //TODO
     /** Hajper **/ //VPN not finished
     public static final String HAJPER_URL = "https://hajper.se/";
 
+    //TODO
     /** Paf **/ //not finished
     public static final String PAF_URL = "https://www.paf.se/en";
 
     /** AB Trav and Galopp **/
     public static final String TRAVANDGALOPP_URL = "https://www.atg.se/";
 
+    //TODO
     /** 888 Sweden Limited **/
     public static final String SWEDENLIMITED_URL = "https://www.888casino.se/";
 
+    //TODO
     /** Auto Nordic Plc **/ //VPN
     public static final String AUTONORDIC_URL = "https://jallacasino.se/";
 
+    //TODO
     /** Betfair International Ltd **/
     public static final String BETFAIRINTERNATIONAL_URL = "https://www.betfair.se/hu";
 
+    //TODO
     /** Blue Star Planet Limited **/
     public static final String BLUESTARTLIMITED_URL = "https://www.10bet.se/";
 
+    //TODO
     /** Lyckost ApS **/
     public static final String LYCKOST_URL = "https://www.lyckost.se/";
 
@@ -50,33 +59,43 @@ public class EnvironmentUrls {
 
     //region Szlovák kaszinók
 
-    /** NIKÉ **/ //!!!!!!!
-    public static final String NIKE_URL = "https://www.nike.sk/";
+    //TODO
+    /** NIKÉ **/
+    public static final String NIKE_URL = "https://www.nike.sk/casino/hry";
 
-    /** Tipsport **/ //!!!!!!!
-    public static final String TIPSPORT_URL = "https://www.tipsport.sk/";
+    //TODO
+    /** Tipsport **/
+    public static final String TIPSPORT_URL = "https://www.tipsport.sk/casino";
 
+    //TODO
     /** Fortuna **/
     public static final String FORTUNA_URL = "https://www.ifortuna.sk/";
 
+    //TODO
     /** Tipos **/ //!!!!!!!
     public static final String TIPOS_URL = "https://moj.etipos.sk/";
 
+    //TODO
     /** Doxxbet **/
     public static final String DOXXBET_URL = "https://www.doxxbet.sk/";
 
+    //TODO
     /** Monacobet **/
     public static final String MONACOBET_URL = "https://casino.monacobet.sk/";
 
+    //TODO
     /** Kajotwin **///!!!!!!!
     public static final String KAJOTWIN_URL = "https://www.kajotwin.sk/sk/";
 
+    //TODO
     /** Betor **/
     public static final String BETOR_URL = "https://www.betor.sk/";
 
+    //TODO
     /** Synottip **/ //VPN
     public static final String SYNOTTIP_URL = "https://synottip.sk/";
 
+    //TODO
     /** OlyBet **/ //!!!!!!!
     public static final String OLYBET_URL = "https://olybet.sk/";
 
